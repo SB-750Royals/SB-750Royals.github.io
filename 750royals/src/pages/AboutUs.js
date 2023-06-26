b102bdb4bb792c116f9582b14e63f1cc90efca38
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-06-26 18:06:10 -0400

    Finished About Us Page

--- 750royals/src/pages/AboutUs.js
+++ 750royals/src/pages/AboutUs.js
@@ -1,10 +1,27 @@
 import './styles/AboutUs.css';
+import Bio from './components/bio/main.js';
 import React from "react";
+import temp from './images/members/temp.png';
 
 function AboutUs() {
     return (
         <div>
+            <p id="aboutus_title">Meet the team</p>
             <div id="aboutus_background"></div>
+            <div id="aboutus_body">
+                <Bio name="Esha Vigneshwaran" role="President" grade="Class of 2024" image={temp} r='100' g='200' b='100'></Bio>
+                <Bio name="Vignesh Saravanakumar" role="Captain" grade="Class of 2024" image={temp} r='250' g='100' b='100'></Bio>
+                <Bio name="Tejas Raghuram" role="Lead Programmer" grade="Class of 2024" image={temp} r='100' g='100' b='250'></Bio>
+                <Bio name="Abhaya Avalur" role="Lead Notebook" grade="Class of 2024" image={temp} r='100' g='200' b='100'></Bio>
+                <Bio name="Anika Thakur" role="Vice Captain" grade="Class of 2024" image={temp} r='250' g='100' b='100'></Bio>
+                <Bio name="Rakshna Ravishankar" role="Lead Notebook" grade="Class of 2024" image={temp} r='100' g='100' b='250'></Bio>
+                <Bio name="Vivek Banker" role="Lead CAD" grade="Class of 2024" image={temp} r='100' g='200' b='100'></Bio>
+                <Bio name="Yegna Bodepudi" role="Builder" grade="Class of 2024" image={temp} r='250' g='100' b='100'></Bio>
+                <Bio name="Vihaan Pardeshi" role="Programmer" grade="Class of 2024" image={temp}  r='100' g='100' b='250'></Bio>
+                <Bio name="Recruit #1" role="Builder" grade="Class of 2024" image={temp} r='100' g='200' b='100'></Bio>
+                <Bio name="Recruit #2" role="Programmer" grade="Class of 2024" image={temp} r='250' g='100' b='100'></Bio>
+                <Bio name="Recruit #3" role="CAD" grade="Class of 2024" image={temp}  r='100' g='100' b='250'></Bio>
+            </div>
         </div>
     );
 }
--- 750royals/src/pages/Home.js
+++ 750royals/src/pages/Home.js
@@ -5,6 +5,7 @@ function Home() {
     return (
         <div>
             <div id="home_background"></div>
+            
         </div>
     );
 }
